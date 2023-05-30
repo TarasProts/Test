63064e0917a16dfeb9a1122a62f05b3c22f8ced9
taras <taras.prots@staff.dev.huayaygf.io>
2023-05-30 13:26:43 +0300

    added alarm

---
+++ app/src/main/java/com/tapac1k/bootcounter/AlarmReceiver.kt
@@ -0,0 +1,11 @@
+package com.tapac1k.bootcounter
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        //TODO showNotification()
+    }
+}
--- app/src/main/java/com/tapac1k/bootcounter/MainApp.kt
+++ app/src/main/java/com/tapac1k/bootcounter/MainApp.kt
@@ -1,12 +1,25 @@
 package com.tapac1k.bootcounter
 
+import android.app.AlarmManager
 import android.app.Application
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.SystemClock
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainApp: Application() {
+    val delay =  15*60 *1000L
+    val pi = PendingIntent.getBroadcast(this, 0, Intent(this, AlarmReceiver::class.java), PendingIntent.FLAG_IMMUTABLE)
 
-    fun showNotification() {
-
+    override fun onCreate() {
+        super.onCreate()
+        setupAlarm()
+    }
+    fun setupAlarm() {
+        val am = (getSystemService(Service.ALARM_SERVICE) as AlarmManager)
+        am.cancel(pi)
+        am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + delay, delay, pi)
     }
 }
